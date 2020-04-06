8874ba74b8ab98fb68b68db93c19d4e058748965
Songbai Yan <colorguitar@hotmail.com>
2020-04-06 12:41:59 +0800

    add student

--- src/main/java/com/oocl/Student.java
+++ src/main/java/com/oocl/Student.java
@@ -1,4 +1,8 @@
 package com.oocl;
 
-public class Student {
+public class Student extends Person {
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL.", this.getName(), this.getAge());
+    }
 }
--- src/test/java/com/oocl/StudentTest.java
+++ src/test/java/com/oocl/StudentTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
+package com.oocl;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class StudentTest {
+    @Test
+    public void should_return_correct_introduce_given_tom_and_18() {
+        Person student = new Student();
+        student.setName("Tom");
+        student.setAge(18);
 
+        Assert.assertEquals("My name is Tom. I am 18 years old. Coding for the glory of OOCL.", student.introduce());
+    }
 }

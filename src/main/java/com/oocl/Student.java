e497b609e6511facab78eff424b1c26d907d8c02
Songbai Yan <colorguitar@hotmail.com>
2020-04-06 13:14:53 +0800

    add klass

--- src/main/java/com/oocl/Student.java
+++ src/main/java/com/oocl/Student.java
@@ -1,8 +1,19 @@
 package com.oocl;
 
 public class Student extends Person {
+
+    private int klass;
+
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL.", this.getName(), this.getAge());
+        return String.format("My name is %s. I am %s years old. I am a student of class %s. Coding for the glory of OOCL.", this.getName(), this.getAge(), this.klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
     }
 }
--- src/test/java/com/oocl/StudentTest.java
+++ src/test/java/com/oocl/StudentTest.java
@@ -6,10 +6,11 @@ import org.junit.Test;
 public class StudentTest {
     @Test
     public void should_return_correct_introduce_given_tom_and_18() {
-        Person student = new Student();
+        Student student = new Student();
         student.setName("Tom");
         student.setAge(18);
+        student.setKlass(2);
 
-        Assert.assertEquals("My name is Tom. I am 18 years old. Coding for the glory of OOCL.", student.introduce());
+        Assert.assertEquals("My name is Tom. I am 18 years old. I am a student of class 2. Coding for the glory of OOCL.", student.introduce());
     }
 }

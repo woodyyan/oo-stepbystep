46bc4ad0a1a2730486dd27b4976025f7bd57b85c
Songbai Yan <colorguitar@hotmail.com>
2020-04-07 14:32:05 +0800

    add mvn wrapper

--- src/main/java/com/oocl/Student.java
+++
@@ -1,8 +0,0 @@
-package com.oocl;
-
-public class Student extends Person {
-    @Override
-    public String introduce() {
-        return String.format("My name is %s. I am %s years old. Coding for the glory of OOCL.", this.getName(), this.getAge());
-    }
-}
--- src/main/java/com/oocl/Teacher.java
+++
@@ -1,8 +0,0 @@
-package com.oocl;
-
-public class Teacher extends Person {
-    @Override
-    public String introduce() {
-        return String.format("My name is %s. I am %s years old. Teaching for the future of world.", this.getName(), this.getAge());
-    }
-}
